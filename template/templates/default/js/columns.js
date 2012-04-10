6b7977f16de17256a82db615f28b6f2d9a118af1
Jonathan Bennett <jbennett@ccistudios.com>
2012-04-10 10:12:36 -0400

    Added script init script
    Conflicts:
    
    	template/templates/default/js/columns.js
    	template/templates/kiwanis-2012/js/lettering.js

--- template/templates/default/js/columns.js
+++ template/templates/default/js/columns.js
@@ -17,21 +17,18 @@ CCI.Columns = new Class({
 		this.selector = selector;
 		this.columns = this.container.getElements(this.selector);
 		
-		this.offset = 52;
+		this.offset = 20;
 		this.height = 0;
 		
 		for (i = this.columns.length - 1; i >= 0; i--) {
 			height = this.columns[i].getSize().y - this.offset;
 			if (height > this.height) {
 				this.height = height;
+				console.log(height);
 			}
 		}
 		
 		this.columns.setStyle('height', this.height);
 	}
 	
-});
-
-window.addEvent('load', function () {
-	c = new CCI.Columns($('bottom'), '.moduletable');
 });
