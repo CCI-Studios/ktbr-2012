8f9bd133dd5bd22ffeee5a5a7c10f7cbb47d5b80
Jonathan Bennett <jbennett@ccistudios.com>
2012-04-18 11:37:56 -0400

    Made 2nd rollover optional

--- template/templates/default/js/script-init.js
+++ template/templates/default/js/script-init.js
@@ -1,7 +1,7 @@
 window.addEvent('domready', function() {
 	// image roll overs
 	new CCI.Rollover('img.rollover');
-	new CCI.Rollover('li.item-122 img, li.item-105 img', { normal_text: '.', over_text: '_rollover.'});
+	// new CCI.Rollover('li.item-122 img, li.item-105 img', { normal_text: '.', over_text: '_rollover.'});
 
 	// image lettering
 	// $$('h1, h2, h3, h4, h5, h6').lettering('words');
