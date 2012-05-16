ae76c5dcb9c2bfde46494c72b6fd8623e6cd0d74
Matt Elliott <melliott@ccistudios.com>
2012-05-16 10:50:02 -0400

    deploy setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,14 +3,14 @@ set :stages, %w(staging production)
 set :default_stage, "staging"
 require "capistrano/ext/multistage"
 
-set :application, "APPLICATION_NAME"
+set :application, "keeptheballrolling.ca"
 
 # repository info
-set :repository,  "git@github.com:CCI-Studios/PROJECT.git"
+set :repository,  "git@github.com:CCI-Studios/ktbr-2012.git"
 set :scm, :git
 
 # ssh settings
-set :user, "USERNAME"
+set :user, "ktbr"
 set :use_sudo, false
 
 # Joomla
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,10 +1,10 @@
 # repository info
-set :branch, "development"
+set :branch, "master"
 
 # This may be the same as your `Web` server
-role :app, "ADDRESS"
+role :app, "ccistudios.com"
 
 # directories
-set :deploy_to, "/home/ACCOUNT/subdomains/DOMAIN"
+set :deploy_to, "/home/ktbr/subdomains/dev"
 set :public, "#{deploy_to}/public_html"
 set :extensions, %w[public template]
