d877d43274b43b40930c4998777747f7458159fe
Matt Elliott <melliott@ccistudios.com>
2012-06-26 09:17:55 -0400

    viewport width set to device width, no scroll.

--- template/templates/KTBR/index.php
+++ template/templates/KTBR/index.php
@@ -35,7 +35,7 @@ $typekit = null;
 
  	<jdoc:include type="head" />
 
-	<meta name="viewport" content="width=940px, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 	<link rel="icon" type="image/x-icon" href="/templates/<?= $this->template ?>/resources/favicon.ico">
 	<link rel="shortcut icon" type="image/x-icon" href="/templates/OAB/favicon.ico">	
 	<link rel="apple-touch-icon" href="/templates/<?= $this->template ?>/resources/apple-touch-icon.png">
